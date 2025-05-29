163879010545ce16b370812a084de45d5c8f3c08
devAugustoW <augustodantas.dev@gmail.com>
2025-05-29 16:07:12 -0300

    feat: testing dotnev

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,18 @@
 import express from 'express';
-import { config } from './config/env';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-// Middleware para aceitar JSON
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const APP_NAME = process.env.APP_NAME;
 
 app.get('/', (req, res) => {
-  res.json({ 
-    message: "🎉 Mediotech API funcionando!",
-    environment: config.nodeEnv,
-    timestamp: new Date().toISOString()
-  });
-});
-
-app.get('/health', (req, res) => {
   res.json({
-    status: 'OK',
-    message: '✅ API saudável e funcionando!',
-    port: config.port
-  });
-});
+    message: `${APP_NAME} funcionando!`,
+  })
+})
 
-app.listen(config.port, () => {
-  console.log(`🚀 Servidor Mediotech rodando em http://localhost:${config.port}`);
-  console.log(`🌍 Ambiente: ${config.nodeEnv}`);
-});
+app.listen(PORT, () => {
+  console.log(`${APP_NAME} rodando em http://localhost:${PORT}`);
+})
